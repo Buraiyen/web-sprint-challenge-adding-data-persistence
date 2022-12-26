21c0ff6fd5095b0f858a141231da57e17e09fb20
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-26 12:15:20 -0800

    feat: server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,5 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+const server = require('./api/server');
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`
+********************************\n
+SERVER STARTED ON PORT ${port} \n
+********************************`);
+});
