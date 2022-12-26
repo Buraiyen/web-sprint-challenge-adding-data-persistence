240cc1bf7eb0ff9181559e4f5cfa38492eeec338
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-26 11:07:26 -0800

    feat: migration for projects table

---
+++ data/migrations/20221226190043_create-tables.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+  return knex.schema.createTable('projects', (tbl) => {
+    tbl.increments('project_id');
+    tbl.string('project_name').notNullable();
+    tbl.string('project_description');
+    tbl.boolean('project_completed').defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('projects');
+};
