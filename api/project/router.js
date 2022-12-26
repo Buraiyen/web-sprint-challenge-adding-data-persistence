0f7caeca662e39d572c0999496974d74619dea3a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-26 12:50:44 -0800

    feat: GET all projects

--- api/project/router.js
+++ api/project/router.js
@@ -1,9 +1,24 @@
 // build your `/api/projects` router here
 const express = require('express');
 const router = express.Router();
+const Projects = require('./model');
 
+// GET all projects
 router.get('/', (req, res) => {
-  console.log('test project');
+  Projects.getAll().then((projects) => {
+    // SQLite3 defines booleans as either 0 or 1
+    // This loop replaces the project_completed values to
+    // either true or false
+    projects.forEach((project) => {
+      if (project.project_completed) {
+        project.project_completed = true;
+      } else {
+        project.project_completed = false;
+      }
+    });
+
+    res.status(200).json(projects);
+  });
 });
 
 module.exports = router;
