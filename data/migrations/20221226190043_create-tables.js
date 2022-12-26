35a082539eee77e8fce10ba1919714e0a643c50b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-26 11:19:17 -0800

    feat: tasks table migration

--- data/migrations/20221226190043_create-tables.js
+++ data/migrations/20221226190043_create-tables.js
@@ -14,6 +14,18 @@ exports.up = (knex) => {
       tbl.increments('resource_id');
       tbl.string('resource_name').notNullable().unique();
       tbl.string('resource_description');
+    })
+    .createTable('tasks', (tbl) => {
+      tbl.increments('task_id');
+      tbl.string('task_description').notNullable();
+      tbl.string('task_notes');
+      tbl.boolean('task_completed').defaultTo(false);
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects');
     });
 };
 
@@ -23,6 +35,7 @@ exports.up = (knex) => {
  */
 exports.down = (knex) => {
   return knex.schema
+    .dropTableIfExists('tasks')
     .dropTableIfExists('resources')
     .dropTableIfExists('projects');
 };
