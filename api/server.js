0df31955ab106c1e5c75faf01c5afa7c62be0f2e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-26 12:22:40 -0800

    feat: projects router

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 // build your server here and require it from index.js
 const express = require('express');
 const server = express();
+const projectsRouter = require('./project/router');
 
+server.use('/api/projects', projectsRouter);
 module.exports = server;
