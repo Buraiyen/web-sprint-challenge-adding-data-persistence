2caf02667ede4b635fc491b488ab9f81f6db03ae
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-27 11:42:53 -0800

    feat: GET resources

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,12 @@
 // build your `/api/resources` router here
+const express = require('express');
+const router = express.Router();
+const Resources = require('./model');
+
+router.get('/', (req, res) => {
+  Resources.getAll().then((resources) => {
+    res.status(200).json(resources);
+  });
+});
+
+module.exports = router;
