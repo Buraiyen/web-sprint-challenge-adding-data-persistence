cecd376daa139a074617ad152a6a02c635f4fc1f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-27 12:24:08 -0800

    feat: tasks router

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,10 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const router = express.Router();
+const Tasks = require('./model');
+
+router.get('/', (req, res) => {
+  console.log('tasks');
+});
+
+module.exports = router;
